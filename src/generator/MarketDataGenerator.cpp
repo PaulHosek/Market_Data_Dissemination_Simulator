8fe7b46b81d0cdf564c21583d97859d55ac88644
Paul Hosek <paul@hosek.de>
2025-05-11 13:46:49 +0200

    Added Concrete generator class and wrote symbol reading function & constructor

--- src/generator/MarketDataGenerator.cpp
+++ src/generator/MarketDataGenerator.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "MarketDataGenerator.h"
+
+#include <fstream>
+
+void MarketDataGenerator::MarketDataGenerator() {
+
+}
+
+void MarketDataGenerator::setup(uint32_t message_per_sec) {
+    // read symbols file
+
+    this->message_per_sec = message_per_sec;
+    // set messages per second
+
+}
+
+
+/**
+ * Reads a list of stock tickers from a file.
+ * Each ticker is expected to be on a separate line, with a maximum length of 8 characters.
+ *
+ * @param filename The path to the file containing the tickers.
+ * @return A vector of strings representing the stock tickers.
+ *
+ * Note: This implementation uses std::ifstream for file reading.
+ * Using a memory-mapped file (MMF) is an alternative approach for large files.
+ */
+std::vector<std::string> MarketDataGenerator::read_symbols_file(std::string const& filename) {
+    std::vector<std::string> tickers;
+    std::ifstream file(filename);
+    std::string str;
+    while (std::getline(file, str)) {
+        if (!str.empty() && str.length() < 9) {
+           tickers.push_back(str);
+        }
+    }
+    return tickers;
+}
--- src/generator/MarketDataGenerator.h
+++ src/generator/MarketDataGenerator.h
@@ -4,10 +4,28 @@
 
 #ifndef MARKETDATAGENERATOR_H
 #define MARKETDATAGENERATOR_H
+#include <filesystem>
+
+#include "IGenerator.h"
+#include <vector>
+#include <string>
 
 
+class MarketDataGenerator : public IGenerator{
+public:
 
-class MarketDataGenerator {
+    MarketDataGenerator(messages_per_second, symbols_file, ticker_fname)
+        : symbols{read_symbols_file(ticker_fname)},
+        message_per_sec{messages_per_second},
+        symbols_filepath{symbols_file} {};
+    void setup(uint32_t message_per_sec) override;
+
+
+private:
+    const std::vector<const std::string> symbols;
+    uint32_t message_per_sec;
+    std::filesystem::path symbols_filepath;
+    static std::vector<std::string> read_symbols_file(std::string const& filename);
 
 };
 
